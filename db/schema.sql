3d9bd2030dcf364c611e0def69dabbc1a2243612
courtneyhook <118090383+courtneyhook@users.noreply.github.com>
2024-03-15 21:35:12 -0500

    created tables

--- db/schema.sql
+++ db/schema.sql
@@ -3,3 +3,22 @@ CREATE DATABASE company_db;
 
 USE company_db;
 
+CREATE TABLE department (
+    id      INT PRIMARY KEY,
+    name    VARCHAR(30) NOT NULL /*to hold department name*/
+);
+
+CREATE TABLE role (
+    id               INT PRIMARY KEY,
+    title            VARCHAR(30),  /*to hold role title*/
+    salary           DECIMAL, /*to hold role salary*/
+    department_id    INT /*to hold reference to department the role belongs to*/
+);
+
+CREATE TABLE employee (
+    id              INT PRIMARY KEY,
+    first_name      VARCHAR(30),  /*to hold employee first name*/
+    last_name       VARCHAR(30), /*to hold employee last name*/
+    role_id         INT,  /*to hold reference to employee role*/
+    manager_id      INT /*to hold reference to another employee that is the manager of the current employee ...NULL if the employee has no manager*/
+)
